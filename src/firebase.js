5ebd96e31b8a8176d6dacd4725b14f92d33739df
newton <newtonnarzary28@gmail.com>
2023-11-17 00:00:38 +0530

    Final commits

--- src/firebase.js
+++ src/firebase.js
@@ -7,12 +7,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBtI1cP8RW8rSj18Tzira5euc3PNE8iJUM",
-  authDomain: "react-chat-app-41bfa.firebaseapp.com",
-  projectId: "react-chat-app-41bfa",
-  storageBucket: "react-chat-app-41bfa.appspot.com",
-  messagingSenderId: "26356879422",
-  appId: "1:26356879422:web:05c84fe121990fd7cf816f",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
